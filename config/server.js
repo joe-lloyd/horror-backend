832ffdf51472cbb6aa33fa824efb88db10d4c1e3
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2021-04-18 14:51:53 +0200

    base setup sorted + env variables set

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', process.env.HOST),
+  port: env.int('PORT', process.env.PORT),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
+    },
+  },
+});
